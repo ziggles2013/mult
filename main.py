2eb5759a35d0f6d312d78818d017e96656b63ecd
ziggles2013 <zrosent@gmail.com>
2026-08-19 13:49:42 -0400

    Added other stuff to main.py

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def task_01_multiplation_table():
         answer = number * i
         print(f"{number} x {i} = {answer}")
 
+
+def task_01_STRECH_full_grid():
+    print("-----full multiplation table-----")
+
+    for row in range(1, 11):
+        for col in  range(1, 11):
+            product = row * col
+            print(f"{product:4}", end='')
+        print()
+
